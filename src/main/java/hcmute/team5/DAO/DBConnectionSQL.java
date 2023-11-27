75c1a6fd3ab4382aa58d6ef9fc9a3df5b1b33f3b
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2023-11-27 15:53:50 +0700

    Update DBConnectionSQL.java

--- src/main/java/hcmute/team5/DAO/DBConnectionSQL.java
+++ src/main/java/hcmute/team5/DAO/DBConnectionSQL.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBConnectionSQL {
     private final String serverName = "MINHIEU";
-    private final String dbName = "aaa";
+    private final String dbName = "Hieu";
     private final String portNumber = "1433";
     private final String instance = "";// MSSQLSERVER LEAVE THIS ONE
     private final String userID = "sa";
