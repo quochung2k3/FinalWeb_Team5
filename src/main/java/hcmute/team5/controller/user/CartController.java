179ccc90cd77ab2a11df2f270d39c977255411e6
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2023-12-11 23:53:55 +0700

    a

--- src/main/java/hcmute/team5/controller/user/CartController.java
+++ src/main/java/hcmute/team5/controller/user/CartController.java
@@ -21,7 +21,6 @@ public class CartController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String url = req.getRequestURI();
-        AccountModel account = (AccountModel) req.getSession(false).getAttribute("account");
         if(url.contains("user-cart")) {
             findAll(req, resp);
         }
