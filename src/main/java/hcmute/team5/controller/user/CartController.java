7b4936a83e5bb34a314da814fb7ac2883c5a13e1
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2023-12-11 10:38:41 +0700

    update cart

---
+++ src/main/java/hcmute/team5/controller/user/CartController.java
@@ -0,0 +1,31 @@
+package hcmute.team5.controller.user;
+
+import hcmute.team5.model.AccountModel;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/user-cart"})
+public class CartController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = req.getRequestURI();
+        AccountModel account = (AccountModel) req.getSession(false).getAttribute("account");
+        if(url.contains("user-cart")) {
+            req.setAttribute("name", account.getUserName());
+            RequestDispatcher rd = req.getRequestDispatcher("/views/user/cart.jsp");
+            rd.forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
--- target/test-1.0/decorators/user.jsp
+++ target/test-1.0/decorators/user.jsp
@@ -26,6 +26,21 @@
     <link href=<c:url value="/template/user/css/main.css" />  rel="stylesheet" />
     <link href=<c:url value="/template/user/css/base.css" />  rel="stylesheet" />
 
+    <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+            rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"
+            rel="stylesheet"
+    />
+
 </head>
 <body>
     <!-- Header-->
