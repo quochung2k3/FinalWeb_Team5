513fb507de7907700020e5b942be288499d68ede
phaminhieu30 <151518508+phaminhieu30@users.noreply.github.com>
2023-12-15 09:25:25 +0700

    a

--- src/main/java/hcmute/team5/DAO/DBConnectionSQL.java
+++ src/main/java/hcmute/team5/DAO/DBConnectionSQL.java
@@ -4,12 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnectionSQL {
-    private final String serverName = "DESKTOP-8C0E5N3";
+    private final String serverName = "MINHIEU";
     private final String dbName = "FinalWeb2";
     private final String portNumber = "1433";
     private final String instance = "";// MSSQLSERVER LEAVE THIS ONE
     private final String userID = "sa";
-    private final String password = "nnmvpqh<302";
+    private final String password = "hieu300303";
 
     public Connection getConnection() throws Exception {
         String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
